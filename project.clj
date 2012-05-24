445042bc42bdf6b43975401158cdf39f0e5b4257
Matt Courtney <macourtney@gmail.com>
2012-05-24 15:04:29 -0400

    Updated the version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-internationalization "1.0.0-SNAPSHOT"
+(defproject clj-internationalization "1.0.0"
   :description "Another internationalization library for clojure. clj-internationalization reads a resource bundle and generates functions for each key."
   :dependencies [[clojure-tools "1.1.1"]
                  [org.clojure/clojure "1.3.0"]]
