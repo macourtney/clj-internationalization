3bb8d8223d58806eb781e56488d3270b43e98d4f
Matthew Courtney <macourtney@gmail.com>
2012-11-21 13:32:22 -0500

    Removed the clojure reference in project.clj and updated .gitignore for use with juffedit.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clj-internationalization "1.0.1-SNAPSHOT"
   :description "Another internationalization library for clojure. clj-internationalization reads a resource bundle and generates functions for each key."
-  :dependencies [[clojure-tools "1.1.1"]
-                 [org.clojure/clojure "1.3.0"]]
+  :dependencies [[clojure-tools "1.1.1"]]
 
   :resource-paths ["resources"])
