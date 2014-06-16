d81671a695fcb5bacb6f56f9303c929284204a64
Matt Courtney <macourtney@gmail.com>
2014-06-15 19:55:49 -0700

    Updated the clojure-tools lib to 1.1.2.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-internationalization "1.0.2-SNAPSHOT"
   :description "Another internationalization library for clojure. clj-internationalization reads a resource bundle and generates functions for each key."
-  :dependencies [[clojure-tools "1.1.1"]]
+  :dependencies [[clojure-tools "1.1.2"]]
 
   :resource-paths ["resources"])
